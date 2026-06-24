c1efb96f55c326abce21b268de06e999cb72a1d6
Harukiansyah <119049542+Harukiansyah@users.noreply.github.com>
2026-06-24 16:20:39 +0700

    Update ff_tweak.sh

--- patches/ff_tweak.sh
+++ patches/ff_tweak.sh
@@ -1,16 +1,7 @@
 #!/bin/bash
-# Ngasih tau kita folder apa aja yang ada di sana
-echo "Daftar folder yang ada:"
-ls -F
+# Hapus semua perintah 'cd'
+# Langsung tembak arch/ dari root folder
 
-# Coba masuk ke folder yang kemungkinan besar adalah source code
-# Kalau common gak ada, nanti kita liat di log folder apa yang muncul
-cd common || cd android-kernel || cd kernel || echo "Folder source code gak ketemu!"
-
-echo "Sekarang berada di:"
-pwd
-
-# Baru jalanin tweak-nya
 echo "CONFIG_HZ_1000=y" >> arch/arm64/configs/gki_defconfig
 echo "CONFIG_HZ=1000" >> arch/arm64/configs/gki_defconfig
 echo "CONFIG_TCP_CONG_BBR=y" >> arch/arm64/configs/gki_defconfig
@@ -18,3 +9,5 @@ echo "CONFIG_DEFAULT_BBR=y" >> arch/arm64/configs/gki_defconfig
 echo "CONFIG_DEFAULT_TCP_CONG=\"bbr\"" >> arch/arm64/configs/gki_defconfig
 echo "CONFIG_NET_SCH_FQ=y" >> arch/arm64/configs/gki_defconfig
 echo "CONFIG_CPU_FREQ_GOV_SCHEDUTIL=y" >> arch/arm64/configs/gki_defconfig
+
+echo "Gaming Tweak Install Successfully!"
