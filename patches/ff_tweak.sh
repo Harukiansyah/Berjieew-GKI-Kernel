a3eaaf423ad11f1eb743adba89b009b30b4a918b
Harukiansyah <119049542+Harukiansyah@users.noreply.github.com>
2026-06-24 15:04:43 +0700

    Update ff_tweak.sh

--- patches/ff_tweak.sh
+++ patches/ff_tweak.sh
@@ -1,1 +1,9 @@
 
+# Tweak Gaming Free Fire & CODM
+echo "CONFIG_HZ_1000=y" >> arch/arm64/configs/gki_defconfig
+echo "CONFIG_HZ=1000" >> arch/arm64/configs/gki_defconfig
+echo "CONFIG_TCP_CONG_BBR=y" >> arch/arm64/configs/gki_defconfig
+echo "CONFIG_DEFAULT_BBR=y" >> arch/arm64/configs/gki_defconfig
+echo "CONFIG_DEFAULT_TCP_CONG=\"bbr\"" >> arch/arm64/configs/gki_defconfig
+echo "CONFIG_NET_SCH_FQ=y" >> arch/arm64/configs/gki_defconfig
+echo "CONFIG_CPU_FREQ_GOV_SCHEDUTIL=y" >> arch/arm64/configs/gki_defconfig
